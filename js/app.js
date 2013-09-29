e3a4236bfbb66e2abc87dc48c319795d670d1e94
Manassarn Manoonchai <manassarn@gmail.com>
2013-09-29 22:24:00 +0700

    Add sharing & url creation

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/:word', { templateUrl: 'partials/main.html', controller: 'MainCtrl' });
+    $routeProvider.when('/', { templateUrl: 'partials/main.html', controller: 'MainCtrl', reloadOnSearch: false });
+    $routeProvider.when('/:word', { redirectTo: function($routeParams, path, search) {
+      return '/?word=' + $routeParams.word;
+    }});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
--- js/controllers.js
+++ js/controllers.js
@@ -5,22 +5,25 @@
 angular.module('myApp.controllers', []).
   controller('MainCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
     $scope.word = $routeParams.word;
+
     $scope.generate = function() {
       html2canvas(document.getElementById("image-wrapper"), {
-          onrendered: function(canvas) {
-            var img=document.createElement("img");
-            img.setAttribute('src', canvas.toDataURL("image/png"));
-            document.body.appendChild(img);
+        onrendered: function(canvas) {
+          var img=document.createElement("img");
+          img.setAttribute('src', canvas.toDataURL("image/png"));
+          document.body.appendChild(img);
 
-            document.getElementById("image-wrapper").innerHTML = "";
-            document.getElementById("image-wrapper").appendChild(img);
+          document.getElementById("image-wrapper").innerHTML = "";
+          document.getElementById("image-wrapper").appendChild(img);
 
-            document.getElementById("submit").innerHTML = '<div class="row"><button disabled class="btn btn-disabled col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">Done!</button></div>';
+          document.getElementById("submit").innerHTML = '<div class="row"><button disabled class="btn btn-disabled col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">Done!</button></div>';
 
-            alert("Done! Now you can save the image");
-          }
+          alert("Done! Now you can save the image");
+
+          window.location.hash = "/?word=" + encodeURIComponent($scope.word);
+          document.getElementById("input").value = window.location.href;
+        }
       });
-
-      return false;
     }
+
   }])
