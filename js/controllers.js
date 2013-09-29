af59833304bfd4627a199d2e25101a08e54eda23
Manassarn Manoonchai <manassarn@gmail.com>
2013-09-29 18:09:47 +0700

    Added alert

--- js/controllers.js
+++ js/controllers.js
@@ -16,6 +16,8 @@ angular.module('myApp.controllers', []).
             document.getElementById("image-wrapper").appendChild(img);
 
             document.getElementById("submit").innerHTML = '<div class="row"><button disabled class="btn btn-disabled col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">Done!</button></div>';
+
+            alert("Done! Now you can save the image");
           }
       });
 
